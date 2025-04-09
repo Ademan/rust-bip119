10d82853af44fa63cd9a7f219449c11e19bd5cc6
Daniel Roberts <ademan555@gmail.com>
2025-04-09 13:24:58 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: CC0-1.0
+
+//! BIP-119 CHECKTEMPLATEVERIFY
+//!
+//! Implementation of BIP-119 default template hash calculation, as defined at
+//! <https://github.com/bitcoin/bips/blob/master/bip-0119.mediawiki>
+
+use bitcoin::hashes::{hash_newtype, Hash, sha256};
+use bitcoin::io::Write;
+
+use bitcoin::{
+    consensus::Decodable,
+    consensus::Encodable,
+    hashes::sha256::Hash as Sha256,
+    Transaction,
+};
+
+pub use bitcoin::opcodes::all::OP_NOP4 as OP_CHECKTEMPLATEVERIFY;
+
+hash_newtype! {
+    /// Default CHECKTEMPLATEVERIFY hash of a transaction
+    #[hash_newtype(forward)]
+    pub struct DefaultCheckTemplateVerifyHash(sha256::Hash);
+}
+
+impl From<DefaultCheckTemplateVerifyHash> for bitcoin::secp256k1::Message {
+    fn from(hash: DefaultCheckTemplateVerifyHash) -> bitcoin::secp256k1::Message {
+        bitcoin::secp256k1::Message::from_digest(hash.to_byte_array())
+    }
+}
+
+impl Encodable for DefaultCheckTemplateVerifyHash {
+    fn consensus_encode<W: Write + ?Sized>(&self, w: &mut W) -> Result<usize, bitcoin::io::Error> {
+        self.as_byte_array().consensus_encode(w)
+    }
+}
+
+impl Decodable for DefaultCheckTemplateVerifyHash {
+    fn consensus_decode<R: bitcoin::io::Read + ?Sized>(reader: &mut R) -> Result<Self, bitcoin::consensus::encode::Error> {
+        Ok(Self::from_byte_array(<<Self as Hash>::Bytes>::consensus_decode(reader)?))
+    }
+}
+
+const CTV_ENC_EXPECT_MSG: &str = "hash writes are infallible";
+
+impl DefaultCheckTemplateVerifyHash {
+    /// Calculate the BIP-119 default template for a transaction at a particular input index
+    pub fn new(transaction: &Transaction, input_index: u32) -> Self {
+        // Since Sha256::write() won't fail and consensus_encode() guarantees to never
+        // fail unless the underlying Write::write() fails, we don't need to worry about
+        // fallibility
+        let mut sha256 = Sha256::engine();
+
+        transaction.version.consensus_encode(&mut sha256).expect(CTV_ENC_EXPECT_MSG);
+        transaction.lock_time.consensus_encode(&mut sha256).expect(CTV_ENC_EXPECT_MSG);
+
+        let any_script_sigs = transaction.input.iter()
+            .any(|input| !input.script_sig.is_empty());
+
+        if any_script_sigs {
+            let mut script_sig_sha256 = Sha256::engine();
+
+            for input in transaction.input.iter() {
+                input.script_sig.consensus_encode(&mut script_sig_sha256).expect(CTV_ENC_EXPECT_MSG);
+            }
+
+            let script_sig_sha256 = Sha256::from_engine(script_sig_sha256);
+            script_sig_sha256.consensus_encode(&mut sha256).expect(CTV_ENC_EXPECT_MSG);
+        }
+
+        let vin_count: u32 = transaction.input.len() as u32;
+        sha256.write(&vin_count.to_le_bytes()).expect(CTV_ENC_EXPECT_MSG);
+
+        {
+            let mut sequences_sha256 = Sha256::engine();
+            for input in transaction.input.iter() {
+                let sequence: u32 = input.sequence.to_consensus_u32();
+                sequences_sha256.write(&sequence.to_le_bytes()).expect(CTV_ENC_EXPECT_MSG);
+            }
+            let sequences_sha256 = Sha256::from_engine(sequences_sha256);
+            sequences_sha256.consensus_encode(&mut sha256).expect(CTV_ENC_EXPECT_MSG);
+        }
+
+        let vout_count: u32 = transaction.output.len() as u32;
+        sha256.write(&vout_count.to_le_bytes()).expect(CTV_ENC_EXPECT_MSG);
+
+        {
+            let mut outputs_sha256 = Sha256::engine();
+            for output in transaction.output.iter() {
+                output.consensus_encode(&mut outputs_sha256).expect(CTV_ENC_EXPECT_MSG);
+            }
+
+            let outputs_sha256 = Sha256::from_engine(outputs_sha256);
+            outputs_sha256.consensus_encode(&mut sha256).expect(CTV_ENC_EXPECT_MSG);
+        }
+
+        sha256.write(&input_index.to_le_bytes()).expect(CTV_ENC_EXPECT_MSG);
+
+        DefaultCheckTemplateVerifyHash(
+            Sha256::from_engine(sha256)
+        )
+    }
+}
---
+++ tests/bip_119.rs
@@ -0,0 +1,108 @@
+//! Tests CTV test vectors from BIP 119
+//! defined at <https://github.com/bitcoin/bips/blob/master/bip-0119/vectors/ctvhash.json>
+
+#![cfg(feature = "serde")]
+
+// FIXME: sort imports
+use bip119::DefaultCheckTemplateVerifyHash;
+use bitcoin::secp256k1::{Secp256k1, SecretKey};
+use bitcoin::{
+    io::Cursor,
+    hex::FromHex,
+};
+use bitcoin::;
+use bitcoin::hashes::Hash;
+use bitcoin::Transaction;
+use bitcoin::consensus::{Encodable,Decodable};
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::str::FromStr;
+
+#[derive(Debug,Deserialize)]
+struct CtvTestVector {
+    #[serde(rename = "hex_tx", with = "bitcoin::consensus::serde::With::<bitcoin::consensus::serde::Hex>")]
+    transaction: Transaction,
+
+    spend_index: Vec<u32>,
+
+    result: Vec<DefaultCheckTemplateVerifyHash>,
+
+    #[serde(flatten)]
+    _remainder: HashMap<String, serde_json::Value>,
+}
+
+#[derive(Debug,Deserialize)]
+#[serde(untagged)]
+enum CtvTestVectorEntry {
+    TestVector(CtvTestVector),
+
+    #[allow(dead_code)]
+    Documentation(String),
+}
+
+fn get_ctv_test_vectors() -> impl Iterator<Item=(Transaction, u32, DefaultCheckTemplateVerifyHash)> {
+    let ctv_test_vectors = include_str!("data/ctvhash.json");
+    let ctv_test_vectors: Vec<CtvTestVectorEntry> = serde_json::from_str(ctv_test_vectors).expect("failed to parse ctv test vectors");
+
+    ctv_test_vectors.into_iter()
+        .filter_map(|entry| {
+            match entry {
+                CtvTestVectorEntry::Documentation(_) => None,
+                CtvTestVectorEntry::TestVector(entry) => Some(entry),
+            }
+        })
+        .flat_map(|entry| {
+            entry.spend_index.into_iter()
+                .zip(entry.result.into_iter())
+                .map(move |(spend_index, result)| (entry.transaction.clone(), spend_index, result))
+        })
+}
+
+#[test]
+fn test_ctv_hash() {
+    for (tx, index, expected_ctv_hash) in get_ctv_test_vectors() {
+        let ctv_hash = DefaultCheckTemplateVerifyHash::new(&tx, index);
+        assert_eq!(ctv_hash, expected_ctv_hash);
+    }
+}
+
+fn get_ctv_hash_bytes() -> Vec<u8> {
+    // sha256("Activate CTV!")
+    Vec::from_hex("b68f63adb2804e999b1d6bfffe060dc004fb40169fa10cb6a6486ddb42200e65").unwrap()
+}
+
+fn get_ctv_hash() -> DefaultCheckTemplateVerifyHash {
+    DefaultCheckTemplateVerifyHash::from_slice(&get_ctv_hash_bytes()).unwrap()
+}
+
+// Probably a bit gratuitous, the rust-bitcoin internal tests for Hash newtypes ought to cover
+// this, but there's nothing wrong with belt and suspenders
+#[test]
+fn test_consensus_decode_encode() {
+    let ctv_hash_bytes = get_ctv_hash_bytes();
+
+    let decoded_ctv = DefaultCheckTemplateVerifyHash::consensus_decode(&mut Cursor::new(&ctv_hash_bytes))
+        .expect("decode should succeed");
+
+    let ctv_hash = DefaultCheckTemplateVerifyHash::from_slice(ctv_hash_bytes.as_ref()).unwrap();
+
+    assert_eq!(&decoded_ctv, &ctv_hash);
+
+    let mut encoded_bytes = Vec::<u8>::new();
+    ctv_hash.consensus_encode(&mut encoded_bytes)
+        .expect("Consensus encode shouldn't fail");
+
+    assert_eq!(&ctv_hash_bytes, &encoded_bytes);
+}
+
+// Really just asserting that this compiles
+#[test]
+fn test_secp256k1_sign() {
+    let pk = SecretKey::from_str("b68f63adb2804e999b1d6bfffe060dc004fb40169fa10cb6a6486ddb42200e65").unwrap();
+    let ctv_hash = get_ctv_hash();
+    let secp = Secp256k1::new();
+
+    let keypair = pk.keypair(&secp);
+
+    let _signature = secp.sign_schnorr_no_aux_rand(&ctv_hash.into(), &keypair);
+}
